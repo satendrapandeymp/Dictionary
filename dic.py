7300a3d28ebc3d3ca5c1b051cb23a5f797ccca05
satyendra pandey <satendrapandeymp@gmail.com>
2017-11-06 06:52:26 +0530

    threading

--- dic.py
+++ dic.py
@@ -1,8 +1,11 @@
 from MySQLdb import escape_string as thwart
-import MySQLdb, time, easygui
+import MySQLdb, time, easygui, Queue, threading
 
 conn = MySQLdb.connect("localhost","root","  ","mydic")
 c = conn.cursor()
+
+conn1 = MySQLdb.connect("localhost","root","  ","mydic")
+c1 = conn1.cursor()
 
 fieldNames = ["part of speech type", 'meaning']
 
@@ -40,11 +43,45 @@ def update(w, t, m):
     conn.commit()
 
 def matching(word):
-    templist = insertion(word)
-    templist += swaping(word)
-    templist += replaceing(word)
-    templist += deletion(word)
-    return final(templist)
+    temp1 = insertion(word)
+    temp2 = swaping(word)
+    temp3 = replaceing(word)
+    temp4 = deletion(word)
+    total = temp3 + temp4 + temp1 + temp2
+
+    num = len(total)/2
+
+    first = total[:num]
+    second = total[num : ]  
+
+    queue = Queue.Queue()
+    threads_list = list()
+    result = []
+    
+    start_time = time.time()
+
+    try:
+        thread1 = threading.Thread(target=final, name="Thread1", args=[first, queue],)
+        thread2 = threading.Thread(target=final1, name="Thread2", args=[second, queue],)
+       
+        threads_list.append(thread1)
+        threads_list.append(thread2)
+
+        for thread in threads_list:
+            thread.start()
+
+        for thread in threads_list:
+            thread.join()
+
+        while not queue.empty():
+            result += queue.get()
+        
+    except:
+        print "Error: unable to start thread"
+
+    print("--- %s seconds ---" % (time.time() - start_time))
+
+    return result
 
 def insertion(word):
     templist = []
@@ -86,14 +123,23 @@ def replaceing(word):
             templist.append(samp)  
     return templist
 
-def final(wordlist):
+def final(wordlist, queue):
     finallist = []
     for word in wordlist:
         check = c.execute("SELECT * from entries WHERE word = '{0}'".format(word))
         if int(check)>0:
             finallist.append(word)
-    return finallist
+    print finallist
+    queue.put(finallist)
 
+def final1(wordlist, queue):
+    finallist = []
+    for word in wordlist:
+        check = c1.execute("SELECT * from entries WHERE word = '{0}'".format(word))
+        if int(check)>0:
+            finallist.append(word)
+    print finallist
+    queue.put(finallist)
 
 while(True):
     word = easygui.enterbox(msg="Type your word below", title="Dictionary")
